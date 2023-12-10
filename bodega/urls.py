c432ee68d1da6225bc229ebcbdda53754ed40904
Ivan Diaz Lizana <idiazl@bs2.cl>
2023-12-10 04:15:52 -0300

    Se incorpora botón para descargar reporte en formato excel

--- bodega/urls.py
+++ bodega/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('edicionProducto/<codigo>', views.edicionProducto),
     path('editarProducto/', views.editarProducto, name='editarProducto'),
     path('orden_de_compra/', views.orden_de_compra, name='orden_de_compra'),
+    path('formulario_fechas/', views.reporte_facturas, name='reporte'),
+    path('reporte_factura/', views.reporte_facturas, name='detalle_reporte'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bodega/views.py
+++ bodega/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from bodega.models import Productos, Factura
 from django.db.models import F, ExpressionWrapper, DecimalField, Sum, Count
+from django.http import HttpResponseServerError
+from datetime import datetime
 # Create your views here.
 
 
@@ -54,3 +56,55 @@ def orden_de_compra(request):
         
         obtener_factura.save()
     return render(request, "orden_de_compra.html", {'factura':factura})
+
+
+# Reporte de facturas
+
+
+def reporte_facturas(request):
+
+    try:
+        if request.method == 'POST':
+            #Toma los datos de la planilla 
+            fecha_inicio_str = request.POST.get('fecha_inicio')
+            fecha_fin_str = request.POST.get('fecha_fin')
+            #Transforma los datos a formato fecha
+            fecha_inicio = datetime.strptime(fecha_inicio_str, '%Y-%m-%d').date()
+            fecha_fin = datetime.strptime(fecha_fin_str, '%Y-%m-%d').date()
+            #Valida la información de los datos comparandolos con el model
+            facturas = Factura.objects.filter(fecha_factura__range=[fecha_inicio, fecha_fin])
+            # Validación adicional para garantizar que la fecha de inicio sea anterior a la fecha de fin
+            if fecha_inicio > fecha_fin:
+                return render(request, 'error_fechas.html', {'error_message': 'La fecha de inicio debe ser anterior a la fecha de fin'})
+
+            
+            if not facturas:
+                # Modifica el contexto para incluir un mensaje indicando la falta de facturas
+                context = {'mensaje': 'No hay facturas en el rango de fechas seleccionado.', 'fecha_inicio': fecha_inicio, 'fecha_fin': fecha_fin}
+            else:
+                # Si hay facturas, simplemente incluye las facturas en el contexto
+                context = {'facturas': facturas, 'fecha_inicio': fecha_inicio, 'fecha_fin': fecha_fin}
+
+            # Renderiza la misma plantilla 'reporte_facturas.html' con el contexto apropiado
+            return render(request, 'reporte_facturas.html', context)
+
+    except Exception as e:
+        # Manejar otros posibles errores
+        return HttpResponseServerError(f'Error: {e}')
+
+    return render(request, 'formulario_fechas.html')
+
+
+# def reporte_facturas(request):
+    
+#     if request.method == 'POST':
+#         fecha_inicio = request.POST.get('fecha_inicio')
+#         fecha_fin = request.POST.get('fecha_fin')
+
+#         facturas = Factura.objects.filter(fecha__range=[fecha_inicio, fecha_fin])
+
+#         # Puedes procesar los datos de ventas como desees y pasarlos al contexto
+#         context = {'facturas': facturas, 'fecha_inicio': fecha_inicio, 'fecha_fin': fecha_fin}
+#         return render(request, 'reporte_facturas.html', context)
+
+#     return render(request, 'formulario_fechas.html')
